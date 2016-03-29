5526b8a83fb515f62211f36be2bc265edcbaa2f8
Rendy Andrian Saputra <rendyandriansaputra@gmail.com>
2016-03-29 16:00:49 +0700

    data nomor 2
    data nomor 2

---
+++ data.java
@@ -0,0 +1,61 @@
+
+package nomor2;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class data {
+    
+    public void drawPattern(int i ){
+        for (int o = 0; o < 1; o++) {
+            
+        for (int j = 0; j < i; j++) {
+
+                                for (int k = 0; k < i; k++) {
+                                    if ( k >= ( i - j )){
+                                        System.out.print("*");
+                                    }else{
+                                        System.out.print(" ");    
+                                    }
+
+                                }
+
+                                for (int k = 0; k < i; k++) {
+                                    if ( k <= j){
+                                        System.out.print("*");
+                                    }else{
+                                        System.out.print(" ");
+                                    }
+                                }
+                                System.out.println("");
+
+                            }
+
+            for (int j = i; j >= 0; j--) {
+
+                for (int k = 0; k < i; k++) {
+                    if ( k >= ( i - j )){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");    
+                    }
+
+                }
+
+                for (int k = 0; k < i; k++) {
+                    if ( k <= j){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println("");
+
+            }            
+        }
+        
+        
+    }
+    
+}
